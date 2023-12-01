3daf30d7815cfdab848c2d6a47a38d3a98260020
GabiCmg <gabriela.camaroli@gmail.com>
2023-11-30 23:04:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace Invoke;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PokemonGet("gengar");
+    }
+
+    private static void PokemonGet(string nome)
+    {
+        var cliente = new RestClient($"https://pokeapi.co/api/v2/pokemon/{nome}");
+        RestRequest request = new RestRequest("", Method.Get);
+        var response = cliente.Execute(request);
+        Console.WriteLine(response.Content);
+        Console.ReadKey();
+    }
+}
